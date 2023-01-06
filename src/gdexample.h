35cee8b3589a5bd63a930eb555b84ec41ca039e2
devinbayly <baylyd@arizona.edu>
2023-01-06 16:30:35 -0700

    committing the demo folder

--- src/gdexample.h
+++ src/gdexample.h
@@ -2,29 +2,49 @@
 #define GDEXAMPLE_H
 
 #include <Godot.hpp>
-#include <Sprite.hpp>
+#include <File.hpp>
+#include <vector>
+#include <chrono>
+#include <iostream>
+#include <ResourceLoader.hpp>
+#include <Material.hpp>
+#include <fstream>
 #include <String.hpp>
+#include <OS.hpp>
+#include <Spatial.hpp>
+#include <ArrayMesh.hpp>
+#include <MeshInstance.hpp>
 
-namespace godot {
+namespace godot
+{
 
-class GDExample : public Sprite {
-    GODOT_CLASS(GDExample, Sprite)
+    class GDExample : public Spatial
+    {
+        GODOT_CLASS(GDExample, Spatial)
 
-private:
-    float time_passed;
-    float amplitude;
-    String path;
-public:
-    static void _register_methods();
+    private:
+        float speed;
+        float amplitude;
+        float time_passed;
+        float time_emit;
+        String file_pth;
 
-    GDExample();
-    ~GDExample();
+    public:
+        static void _register_methods();
 
-    void _init(); // our initializer called by Godot
+        GDExample();
+        ~GDExample();
 
-    void _process(float delta);
-};
+        void _init(); // our initializer called by Godot
+        void _ready();
+        void _process(float delta);
+        void set_speed(float p_speed);
+        float get_speed();
+        void make_cloud();
+        void set_file_path(String pth);
 
+        String get_file_path();
+    };
 }
 
 #endif
