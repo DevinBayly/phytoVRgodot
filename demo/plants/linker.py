f5f26243773c1b793f1ad61aaa3fd9c69eacc376
devinbayly <baylyd@arizona.edu>
2023-01-14 18:28:05 -0700

    merge existing work with previous to december

---
+++ demo/plants/linker.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+import subprocess as sp
+
+plys = sorted((Path.home()/"Downloads/phyto_vr").rglob("*ply"))
+for i,ply in enumerate(plys):
+    sp.run(f"ln -s {ply} plant{i}.ply",shell=True)
